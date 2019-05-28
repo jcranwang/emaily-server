1d28f9719828b768f3e9915dee0b36108a78f1c3
jcranwang <jason.cr.wang@gmail.com>
2019-05-28 23:01:58 +0930

    Change route message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ Start: "Hello" });
+  res.send({ end: "Bye" });
 });
 
 const PORT = process.env.PORT || 3000;
