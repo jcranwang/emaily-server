f10d67d85ebc58cb32e45d781e4fa3bbf245d10d
jcranwang <jason.cr.wang@gmail.com>
2019-05-29 23:53:09 +0930

    Setup passport.js, google OAuth and nodemon

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const app = express();
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/key");
 
-app.get("/", (req, res) => {
-  res.send({ end: "Bye" });
-});
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: "auth/google/callback"
+}, (accessToken, refreshToken, profile, done) => {
+  console.log(accessToken);
+  console.log(refreshToken);
+  console.log(profile);
+}));
+
+app.get("/auth/google", passport.authenticate("google", {
+  scope: ["profile", "email"]
+}));
+
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
