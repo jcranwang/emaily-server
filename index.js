0894759c9fc54034727d37d9ce9af43d08838df6
jcranwang <jason.cr.wang@gmail.com>
2019-05-30 23:14:06 +0930

    Refactor server structure

--- index.js
+++ index.js
@@ -1,24 +1,9 @@
 const express = require("express");
 const app = express();
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("./config/key");
+const authRouter = require("./routers/authRouter");
+require("./services/passport");
 
-passport.use(new GoogleStrategy({
-  clientID: keys.googleClientID,
-  clientSecret: keys.googleClientSecret,
-  callbackURL: "auth/google/callback"
-}, (accessToken, refreshToken, profile, done) => {
-  console.log(accessToken);
-  console.log(refreshToken);
-  console.log(profile);
-}));
-
-app.get("/auth/google", passport.authenticate("google", {
-  scope: ["profile", "email"]
-}));
-
-app.get("/auth/google/callback", passport.authenticate("google"));
+app.use(authRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
---
+++ routers/authRouter.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const passport = require("passport");
+const authRouter = new express.Router();
+
+authRouter.get("/auth/google", passport.authenticate("google", {
+  scope: ["profile", "email"]
+}));
+
+authRouter.get("/auth/google/callback", passport.authenticate("google"));
+
+module.exports = authRouter;
